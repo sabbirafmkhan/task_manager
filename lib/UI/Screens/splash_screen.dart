344e5e02dea8f161081f6e2baee75858b9d85e3b
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-03-14 01:25:56 +0600

    splash screen complete

--- lib/UI/Screens/splash_screen.dart
+++ lib/UI/Screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager/UI/Screens/login_screen.dart';
+import 'package:task_manager/UI/Utils/assets_paths.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,7 +12,33 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SvgPicture.asset("assets/images/screen-back.svg"));
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            AssetsPath.backgroundSvg,
+            fit: BoxFit.cover,
+          ),
+          Center(child: SvgPicture.asset(AssetsPath.logSvg, width: 120)),
+        ],
+      ),
+    );
   }
 }
