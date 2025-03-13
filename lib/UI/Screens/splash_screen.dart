2e46b42f0693fa95fdb09861662848b3bb66ecdf
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-03-14 00:54:32 +0600

    splash screen background svg add

--- lib/UI/Screens/splash_screen.dart
+++ lib/UI/Screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,10 +11,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Splash Screen"),
-      ),
-    );
+    return Scaffold(body: SvgPicture.asset("assets/images/screen-back.svg"));
   }
 }
