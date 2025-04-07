d0bdcb8afe3affc480995147a2ab7be1412c8008
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-04-07 22:35:01 +0600

    API Integration II getRequest

--- lib/data/service/network_client.dart
+++ lib/data/service/network_client.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-
 import 'package:http/http.dart';
+import 'package:logger/logger.dart';
 
 class NetworkResponse {
   final bool isSuccess;
@@ -17,18 +17,38 @@ class NetworkResponse {
 }
 
 class NetworkClient {
+  static final Logger _logger = Logger();
   static Future<NetworkResponse> getRequest({required String url}) async {
-    Uri uri = Uri.parse(url);
-    Response response = await get(uri);
-    if (response.statusCode == 200) {
-      final decodedJson = jsonDecode(response.body);
-      return NetworkResponse(
-        isSuccess: true,
-        statusCode: response.statusCode,
-        data: decodedJson,
+    try {
+      Uri uri = Uri.parse(url);
+      _logger.i("Url => $url");
+      Response response = await get(uri);
+      _logger.i(
+        ""
+        "Status Code: ${response.statusCode}"
+        "Headers: ${response.headers}"
+        "Response: ${response.body}",
       );
-    } else {
-      return NetworkResponse(isSuccess: false, statusCode: response.statusCode);
+      if (response.statusCode == 200) {
+        final decodedJson = jsonDecode(response.body);
+        return NetworkResponse(
+          isSuccess: true,
+          statusCode: response.statusCode,
+          data: decodedJson,
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      _logger.e(e.toString());
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
     }
   }
 }
