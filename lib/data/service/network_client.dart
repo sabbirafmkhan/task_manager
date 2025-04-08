c0331c24f1e40457eb7ed856ba9746f108e90479
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-04-08 21:48:51 +0600

    API Integration II postRequest

--- lib/data/service/network_client.dart
+++ lib/data/service/network_client.dart
@@ -25,6 +25,50 @@ class NetworkClient {
       Response response = await get(uri);
       _logger.i(
         ""
+        "Status Code: ${response.statusCode}\n"
+        "Headers: ${response.headers}\n"
+        "Response: ${response.body}",
+      );
+      if (response.statusCode == 200) {
+        final decodedJson = jsonDecode(response.body);
+        return NetworkResponse(
+          isSuccess: true,
+          statusCode: response.statusCode,
+          data: decodedJson,
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      _logger.e(e.toString());
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static Future<NetworkResponse> postRequest({
+    required String url,
+    Map<String, dynamic>? body,
+  }) async {
+    try {
+      Uri uri = Uri.parse(url);
+      _logger.i(
+        "Url => $url\n"
+        "Body => $body",
+      );
+      Response response = await post(
+        uri,
+        headers: {'Content-type': 'Application/json'},
+        body: jsonEncode(body),
+      );
+      _logger.i(
+        ""
         "Status Code: ${response.statusCode}"
         "Headers: ${response.headers}"
         "Response: ${response.body}",
