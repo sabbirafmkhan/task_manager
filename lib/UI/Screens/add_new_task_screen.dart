54654cb48af78005c588ad968bcd9c05b76fcd51
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-04-22 22:23:18 +0600

    Add new task api

--- lib/UI/Screens/add_new_task_screen.dart
+++ lib/UI/Screens/add_new_task_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/UI/Widgets/centered_circular_progress_indicator.dart';
 import 'package:task_manager/UI/Widgets/screen_background.dart';
+import 'package:task_manager/UI/Widgets/snack_bar_message.dart';
 import 'package:task_manager/UI/Widgets/tm_app_bar.dart';
+import 'package:task_manager/data/service/network_client.dart';
+import 'package:task_manager/data/utils/urls.dart';
 
 class AddNewTaskScreen extends StatefulWidget {
   const AddNewTaskScreen({super.key});
@@ -10,6 +14,12 @@ class AddNewTaskScreen extends StatefulWidget {
 }
 
 class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
+  final TextEditingController _titleTEController = TextEditingController();
+  final TextEditingController _descriptionTEController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _addNewTaskInProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,36 +28,58 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 80),
-                Text(
-                  "Add New Task",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 24),
-                TextField(
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(hintText: "Title"),
-                ),
-                const SizedBox(height: 8),
-                TextField(
-                  maxLines: 6,
-                  decoration: InputDecoration(
-                    hintText: "Description",
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 80),
+                  Text(
+                    "Add New Task",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    controller: _titleTEController,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(hintText: "Title"),
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your title';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _descriptionTEController,
+                    maxLines: 6,
+                    decoration: InputDecoration(
+                      hintText: "Description",
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                    ),
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your description';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Visibility(
+                    visible: _addNewTaskInProgress == false,
+                    replacement: CenteredCircularProgressIndicator(),
+                    child: ElevatedButton(
+                      onPressed: _onTabSubmitButton,
+                      child: Icon(Icons.arrow_circle_right_outlined),
                     ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: _onTabSubmitButton,
-                  child: Icon(Icons.arrow_circle_right_outlined),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -55,5 +87,45 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
     );
   }
 
-  void _onTabSubmitButton() {}
+  void _onTabSubmitButton() {
+    if (_formKey.currentState!.validate()) {
+      _addNewTask();
+    }
+  }
+
+  Future<void> _addNewTask() async {
+    _addNewTaskInProgress = true;
+    setState(() {});
+
+    Map<String, dynamic> requestBody = {
+      'title': _titleTEController.text.trim(),
+      'description': _descriptionTEController.text.trim(),
+      'status': 'New',
+    };
+
+    final NetworkResponse response = await NetworkClient.postRequest(
+      url: Urls.createTaskUrl,
+      body: requestBody,
+    );
+    _addNewTaskInProgress = false;
+    setState(() {});
+    if (response.isSuccess) {
+      _clearTextFields();
+      showSnackBarMessage(context, 'New task added');
+    } else {
+      showSnackBarMessage(context, response.errorMessage);
+    }
+  }
+
+  void _clearTextFields() {
+    _titleTEController.clear();
+    _descriptionTEController.clear();
+  }
+
+  @override
+  void dispose() {
+    _titleTEController.dispose();
+    _descriptionTEController.dispose();
+    super.dispose();
+  }
 }
--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -3,4 +3,5 @@ class Urls {
   static const String registerUrl = '$_baseUrl/Registration';
   static const String loginUrl = '$_baseUrl/Login';
   static const String updateProfileUrl = '$_baseUrl/ProfileUpdate';
+  static const String createTaskUrl = '$_baseUrl/createTask';
 }
