8d49248d7641b0402127577a88423ab3eb50a8ad
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-02 21:57:26 +0600

    ProgressTaskController

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 import 'package:task_manager/UI/controllers/login_controller.dart';
 import 'package:task_manager/UI/controllers/new_task_controller.dart';
+import 'package:task_manager/UI/controllers/progress_task_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
     Get.put(LoginController());
     Get.put(NewTaskController());
+    Get.put(ProgressTaskController());
   }
 }
