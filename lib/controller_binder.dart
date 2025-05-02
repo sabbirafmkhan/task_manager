af0e7a146dee0e3e72c75e453c5cfce73145bd2f
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-02 21:32:04 +0600

    NewTaskController

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:task_manager/UI/controllers/login_controller.dart';
+import 'package:task_manager/UI/controllers/new_task_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
     Get.put(LoginController());
+    Get.put(NewTaskController());
   }
 }
